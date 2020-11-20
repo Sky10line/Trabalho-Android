56aa3faa31670b2f384e73f96063d899c20d5137
otaviow <olima508@gmail.com>
2020-11-20 19:34:51 -0300

    Autenticação de usuário
    Autenticação de usuário usando o firebase

--- codigo/app/src/main/java/com/example/trabalho_android/MainActivity.kt
+++ codigo/app/src/main/java/com/example/trabalho_android/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.trabalho_android
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,10 +17,28 @@ class MainActivity : AppCompatActivity() {
         if (getCurrentUser() == null){
             val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
 
-            
+            val i = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+
+            startActivityForResult(i, 0)
         }
         else{
             Toast.makeText(this,"Bem-Vindo", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0){
+            if (resultCode == Activity.RESULT_OK){
+                Toast.makeText(this,"Bem-Vindo", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                finishAffinity()
+                //DEVERIA FAZER ALGO AO INVÉS DE DERRUBAR A APLICAÇÃO ???
+            }
         }
     }
     //FUNÇÃO QUE VERIFICA SE JÁ EXISTE UM USUÁRIO AUTENTICADO NA APLICAÇÃO
