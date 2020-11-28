10214384c57aa593ca54a7a61decd887b4933dc9
Sky10line <61299872+Sky10line@users.noreply.github.com>
2020-11-27 23:38:30 -0300

    Game activity pronta

--- codigo/app/src/main/java/com/example/trabalho_android/activities/GameActivity.kt
+++ codigo/app/src/main/java/com/example/trabalho_android/activities/GameActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.CheckBox
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
 import androidx.core.view.iterator
 import com.example.trabalho_android.R
 import com.example.trabalho_android.models.Question
@@ -12,40 +14,113 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_game.*
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
     var database: DatabaseReference? = null
     var questionList: List<Question>? = null
-    var i: Int = 0
+    private var i: Int = -1
+    var score: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
         configFirebase()
-        makeQuestion(i)
+
+        finishBtn.text = "Iniciar"
+        Options.alpha = 0f
+        hintBtn.alpha = 0f
+        hintBtn.isEnabled = false
 
         finishBtn.setOnClickListener {
-
-            for(option in Options){
-                if((option as CheckBox).isChecked){
-                    print(option.id)
+            if(i == -1) {
+                finishBtn.text = "Próximo"
+                Options.alpha = 1f
+                hintBtn.alpha = 1f
+                i++
+                makeQuestion(i)
+            } else if (i < questionList?.count() ?: -1){
+                if(check1Answer() == true) {
+                    for (k in 0 until Options.childCount) {
+                        if ((Options.get(k) as CheckBox).isChecked) {
+                            if (checkAnswer(i, k)) {
+                                score += 100
+                                Toast.makeText(this, score.toString(), Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+                    i++
+                    makeQuestion(i)
+                } else {
+                    Toast.makeText(this, questionList?.get(i)?.correctAns?.toString(), Toast.LENGTH_LONG).show()
+//                    Toast.makeText(this, "Assinale 1 resposta apenas", Toast.LENGTH_LONG).show()
                 }
             }
+            else {
+                //fazer o a transição para tela de colocar nome e subir o score
+                if(score < 0){
+                    score = 0
+                }
+            }
+        }
 
-            i++
-            makeQuestion(i)
+        hintBtn.setOnClickListener {
+                val ranNum = Random.nextInt(0,5)
+
+                if(ranNum+1 != questionList?.get(i)?.correctAns){
+                    (Options.get(ranNum) as CheckBox).text = ""
+                } else {
+                    if(ranNum+1 == 5){
+                        (Options.get(ranNum-1) as CheckBox).text = ""
+                    }
+                    else {
+                        (Options.get(ranNum+1) as CheckBox).text = ""
+                    }
+                }
+                score -= 20
+                hintBtn.isEnabled = false
+        }
+
+        giveUpBtn.setOnClickListener {
+            finish()
         }
     }
 
-    fun makeQuestion(i: Int){
-        questionList?.get(i)?.let {
-            question.setText(it.question.toString())
-            ans1.setText(it.ans1.toString())
-            ans2.setText(it.ans2.toString())
-            ans3.setText(it.ans3.toString())
-            ans4.setText(it.ans4.toString())
-            ans5.setText(it.ans5.toString())
+    private fun check1Answer() : Boolean{
+        var answers = 0;
+        for(option in Options){
+            if((option as CheckBox).isChecked){
+                answers += 1
+            }
         }
+
+        if(answers <= 1){
+            return true
+        }
+        return false
+    }
+
+    private fun checkAnswer(fase: Int, resp: Int) : Boolean{
+        if(questionList?.get(fase)?.correctAns == resp+1){
+            return true
+        }
+        return false
+    }
+
+    private fun makeQuestion(i: Int){
+        for(option in Options){
+            (option as CheckBox).isChecked = false
+        }
+
+        questionList?.get(i)?.let {
+            question.setText(it.question)
+            ans1.setText(it.ans1)
+            ans2.setText(it.ans2)
+            ans3.setText(it.ans3)
+            ans4.setText(it.ans4)
+            ans5.setText(it.ans5)
+        }
+        hintBtn.isEnabled = true
     }
 
     fun getCurrentUser(): FirebaseUser?{
@@ -92,10 +167,9 @@ class GameActivity : AppCompatActivity() {
             val ans3 = map.get("ans3") as String
             val ans4 = map.get("ans4") as String
             val ans5 = map.get("ans5") as String
-//            val correctAns = map.get("correctAns") as Int
-print(ans1)
-            val quest = Question(id, question, ans1, ans2, ans3, ans4, ans5, 1)
+            val correctAns = map.get("correctAns") as Long
 
+            val quest = Question(id, question, ans1, ans2, ans3, ans4, ans5, correctAns.toInt())
             questionsList.add(quest)
         }
 
